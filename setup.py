9e02111af38a0dbddb6e7499bd9942fcf6f78016
Andrey Kislyuk <kislyuk@gmail.com>
2022-01-23 13:54:03 -0800

    v0.3.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aurora-data-api",
-    version="0.3.0",
+    version="0.3.1",
     url='https://github.com/chanzuckerberg/aurora-data-api',
     license='Apache Software License',
     author='Andrey Kislyuk',
