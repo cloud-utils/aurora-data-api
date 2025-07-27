21686fdb287e39e457000cac6691c7b3a3b3caf9
tsuga <2888173+tsuga@users.noreply.github.com>
2025-07-27 12:42:03 +0000

    Fix filename typo

---
+++ aurora_data_api/__init__.py
@@ -0,0 +1,24 @@
+# For backward compatibility
+from .sync import connect
+from .sync import SyncAuroraDataAPIClient as AuroraDataAPIClient
+from .sync import SyncAuroraDataAPICursor as AuroraDataAPICursor
+
+# Import all DB-API constants from base
+from .base import (
+    apilevel,
+    threadsafety,
+    paramstyle,
+    Date,
+    Time,
+    Timestamp,
+    DateFromTicks,
+    # TimeFromTicks,
+    TimestampFromTicks,
+    Binary,
+    STRING,
+    BINARY,
+    NUMBER,
+    DATETIME,
+    ROWID,
+    DECIMAL,
+)
